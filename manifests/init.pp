cc75683127766f18df9164b635f12fb937f85134
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-05-02 10:20:52 +0200

    Manage vimrc.local

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,19 @@ class vim {
         require => Package['vim']
     }
 
+    file { '/etc/vim/vimrc':
+        owner => root,
+        group => root,
+        mode => '0644',
+    }
+
+    file { '/etc/vim/vimrc.local':
+        owner => root,
+        group => root,
+        mode => '0644',
+        source => "puppet:///modules/vim/vimrc.local",
+    }
+
     package { 'vim':
         ensure => present,
     }
