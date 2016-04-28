50f461c18d75d63c497b1792e9de460fe206d6a0
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2016-04-28 16:52:16 +0200

    Add some tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,10 +13,11 @@ class vim {
   }
 
   file { '/etc/vim/vimrc.local':
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/vim/vimrc.local',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/vim/vimrc.local',
+    require => Package['vim']
   }
 
   package { 'vim':
